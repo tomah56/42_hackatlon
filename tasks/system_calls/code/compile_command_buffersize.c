c56213df8dc05ac0c51d49aed45b6e8ae419afbb
Alf Jonathan Langhans <alanghan@student.42heilbronn.de>
2022-11-18 18:49:49 +0100

    Add Explanation for compile_command_buffersize

--- tasks/system_calls/code/compile_command_buffersize.c
+++ tasks/system_calls/code/compile_command_buffersize.c
@@ -1,4 +1,5 @@
 /* 
  * TOPIC: Compiler Options
- * TASK: Please write the correct compilation command 
+ * TASK: Please write the correct compilation command for a BUFFER_SIZE of 1024.
+ * Don't forget the typical 42 rules.
  */
