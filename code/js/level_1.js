4e0422cb5a90bfbed2b2b89d31f45e00b82bfcf2
Fredi-B <fredibechtoldt@outlook.com>
2022-11-19 10:53:04 +0100

    updated level_1

---
+++ code/js/level_1.js
@@ -0,0 +1,10 @@
+function showButton()
+{
+    if (document.getElementById("1").value.replaceAll(" ", "") != "")
+    {
+        document.getElementById("nextvisible").style="visiblity: visible";
+        document.getElementById("storytellerAlf").innerHTML = "lore ipsum";
+
+    }
+
+}
