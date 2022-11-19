f4db1fb07c5f2b31bdb49cdd18ddf88bf9d3470b
Alf Jonathan Langhans <alanghan@student.42heilbronn.de>
2022-11-19 11:06:33 +0100

    Add storytelling to level_1

--- code/js/level_1.js
+++ code/js/level_1.js
@@ -3,7 +3,7 @@ function showButton()
     if (document.getElementById("1").value.replaceAll(" ", "") != "")
     {
         document.getElementById("nextvisible").style="visiblity: visible";
-        document.getElementById("storytellerAlf").innerHTML = "lore ipsum";
+        document.getElementById("storytellerAlf").innerHTML = "Great! Now you and Galaxy Ninja Lad know each other! Let's start your adventure.";
 
     }
 
